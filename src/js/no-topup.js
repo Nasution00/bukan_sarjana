6a2e849a66944ac076a1176d0d2305c41cdd7d18
Nasution00 <jakfar.naesty@gmail.com>
2022-07-23 14:34:30 +0700

    initial commit

---
+++ src/js/no-topup.js
@@ -0,0 +1,247 @@
+const account = {
+  account: [
+    {
+      id: "363498446",
+      nickname: "-",
+    },
+    {
+      id: "363179082",
+      nickname: "-",
+    },
+    {
+      id: "363257965",
+      nickname: "-",
+    },
+    {
+      id: "363074735",
+      nickname: "-",
+    },
+    {
+      id: "359596565",
+      nickname: "-",
+    },
+    {
+      id: "359464880",
+      nickname: "-",
+    },
+    {
+      id: "356728564",
+      nickname: "-",
+    },
+    {
+      id: "314598575",
+      nickname: "-",
+    },
+    {
+      id: "314899562",
+      nickname: "-",
+    },
+    {
+      id: "316345739",
+      nickname: "-",
+    },
+    {
+      id: "315712502",
+      nickname: "-",
+    },
+    {
+      id: "316347367",
+      nickname: "-",
+    },
+    {
+      id: "333219806",
+      nickname: "-",
+    },
+    {
+      id: "338295944",
+      nickname: "-",
+    },
+    {
+      id: "336271215",
+      nickname: "-",
+    },
+    {
+      id: "336358006",
+      nickname: "-",
+    },
+    {
+      id: "336382923",
+      nickname: "-",
+    },
+    {
+      id: "336272016",
+      nickname: "-",
+    },
+    {
+      id: "338159099",
+      nickname: "-",
+    },
+    {
+      id: "338144626",
+      nickname: "-",
+    },
+    {
+      id: "339374285",
+      nickname: "-",
+    },
+    {
+      id: "338154380",
+      nickname: "-",
+    },
+    {
+      id: "338889856",
+      nickname: "-",
+    },
+    {
+      id: "338897652",
+      nickname: "-",
+    },
+    {
+      id: "339608145",
+      nickname: "-",
+    },
+    {
+      id: "338881427",
+      nickname: "-",
+    },
+    {
+      id: "339622699",
+      nickname: "-",
+    },
+    {
+      id: "338878114",
+      nickname: "-",
+    },
+    {
+      id: "339372943",
+      nickname: "-",
+    },
+    {
+      id: "338935706",
+      nickname: "-",
+    },
+    {
+      id: "338150017",
+      nickname: "-",
+    },
+    {
+      id: "345120180",
+      nickname: "-",
+    },
+    {
+      id: "343082073",
+      nickname: "-",
+    },
+    {
+      id: "313897577",
+      nickname: "-",
+    },
+    {
+      id: "315707033",
+      nickname: "-",
+    },
+    {
+      id: "338295949",
+      nickname: "-",
+    },
+    {
+      id: "314907244",
+      nickname: "-",
+    },
+    {
+      id: "314586197",
+      nickname: "-",
+    },
+  ],
+};
+
+const tableAccount = document.getElementById("table-account");
+
+for (let i = 0; i < account.account.length; i++) {
+  const id = account.account[i].id;
+  const nickname = account.account[i].nickname;
+  const row = document.createElement("tr");
+  const colCheck = document.createElement("td");
+  const divContainerCheck = document.createElement("div");
+  const divCheck = document.createElement("div");
+  const check = document.createElement("input");
+
+  const colId = document.createElement("td");
+  const divId = document.createElement("div");
+  const pId = document.createElement("p");
+
+  const colNick = document.createElement("td");
+  colNick.setAttribute("class", "");
+
+  const divNick = document.createElement("div");
+  divNick.setAttribute("class", "flex items-center");
+
+  const pNick = document.createElement("p");
+  pNick.setAttribute("class", "text-sm leading-none text-gray-600 ml-2");
+
+  pNick.innerHTML = i + 1;
+
+  divId.setAttribute("class", "flex items-center pl-5");
+  pId.setAttribute("class", "text-base leading-none text-gray-700 mr-2");
+
+  const colCopy = document.createElement("td");
+  colCopy.setAttribute("class", "flex flex-row justify-end mr-2 py-2");
+
+  const btnCopy = document.createElement("button");
+  btnCopy.setAttribute(
+    "class",
+    "ml-auto focus:ring-2 focus:ring-offset-2 focus:ring-red-300 text-sm leading-none text-gray-600 py-3 px-5 bg-gray-100 rounded hover:bg-gray-200 focus:outline-none"
+  );
+  btnCopy.setAttribute("id", "btn-copy" + i);
+
+  btnCopy.innerHTML = "Copy";
+
+  colCopy.appendChild(btnCopy);
+
+  pId.innerHTML = id;
+  divId.appendChild(pId);
+  colId.appendChild(divId);
+
+  divNick.appendChild(pNick);
+  colNick.appendChild(divNick);
+
+  check.setAttribute("type", "checkbox");
+  check.setAttribute("id", "check" + i);
+  check.setAttribute("name", "check" + i);
+  check.setAttribute(
+    "class",
+    "focus:opacity-100 checkbox absolute cursor-pointer w-full h-full"
+  );
+
+  divContainerCheck.setAttribute("class", "ml-5");
+  divCheck.setAttribute(
+    "class",
+    "bg-gray-200 rounded-sm w-5 h-5 flex flex-shrink-0 justify-center items-center relative"
+  );
+
+  divCheck.appendChild(check);
+  divContainerCheck.appendChild(divCheck);
+
+  row.setAttribute("tabindex", i);
+  row.setAttribute("id", "row" + i);
+  row.setAttribute("class", "focus:outline-none rounded");
+  colCheck.appendChild(divContainerCheck);
+  // col.innerHTML = nickname;
+  row.appendChild(colNick);
+  row.appendChild(colId);
+  row.appendChild(colCheck);
+  row.appendChild(colCopy);
+  tableAccount.appendChild(row);
+
+  const isChecked = document.getElementById("check" + i);
+
+  btnCopy.addEventListener("click", () => {
+    navigator.clipboard.writeText(id);
+    isChecked.checked = true;
+    document.getElementById("row" + i).classList.add("bg-indigo-200");
+  });
+
+  isChecked.addEventListener("change", () => {
+    document.getElementById("row" + i).classList.remove("bg-indigo-200");
+  });
+}
---
+++ src/js/topup.js
@@ -0,0 +1,1098 @@
+const account = {
+  account: [
+    {
+      id: "371415062",
+      nickname: "-",
+    },
+    {
+      id: "371416020",
+      nickname: "-",
+    },
+    {
+      id: "371237214",
+      nickname: "-",
+    },
+    {
+      id: "370423898",
+      nickname: "-",
+    },
+    {
+      id: "370446494",
+      nickname: "-",
+    },
+    {
+      id: "370475733",
+      nickname: "-",
+    },
+    {
+      id: "367269506",
+      nickname: "-",
+    },
+    {
+      id: "367902693",
+      nickname: "-",
+    },
+    {
+      id: "367268747",
+      nickname: "-",
+    },
+    {
+      id: "367873447",
+      nickname: "-",
+    },
+    {
+      id: "282456569",
+      nickname: "-",
+    },
+    {
+      id: "357244066",
+      nickname: "-",
+    },
+    {
+      id: "282457261",
+      nickname: "-",
+    },
+    {
+      id: "283433403",
+      nickname: "-",
+    },
+    {
+      id: "283423543",
+      nickname: "-",
+    },
+    {
+      id: "283468376",
+      nickname: "-",
+    },
+
+    {
+      id: "282446274",
+      nickname: "-",
+    },
+    {
+      id: "283461210",
+      nickname: "-",
+    },
+    {
+      id: "283457720",
+      nickname: "-",
+    },
+    {
+      id: "283459361",
+      nickname: "-",
+    },
+    {
+      id: "322898640",
+      nickname: "-",
+    },
+    {
+      id: "333389198",
+      nickname: "-",
+    },
+    {
+      id: "333384114",
+      nickname: "-",
+    },
+    {
+      id: "333390820",
+      nickname: "-",
+    },
+    {
+      id: "333393449",
+      nickname: "-",
+    },
+    {
+      id: "333395838",
+      nickname: "-",
+    },
+    {
+      id: "333397099",
+      nickname: "-",
+    },
+    {
+      id: "333400565",
+      nickname: "-",
+    },
+    {
+      id: "241353311",
+      nickname: "-",
+    },
+    {
+      id: "342916602",
+      nickname: "-",
+    },
+    {
+      id: "342961318",
+      nickname: "-",
+    },
+    {
+      id: "342934328",
+      nickname: "-",
+    },
+    {
+      id: "343584778",
+      nickname: "-",
+    },
+    {
+      id: "343951303",
+      nickname: "-",
+    },
+    {
+      id: "140402140",
+      nickname: "-",
+    },
+    {
+      id: "164399250",
+      nickname: "-",
+    },
+    {
+      id: "140406460",
+      nickname: "-",
+    },
+    {
+      id: "140402670",
+      nickname: "-",
+    },
+    {
+      id: "140154690",
+      nickname: "-",
+    },
+    {
+      id: "120409831",
+      nickname: "-",
+    },
+    {
+      id: "164529971",
+      nickname: "-",
+    },
+    {
+      id: "136736791",
+      nickname: "-",
+    },
+    {
+      id: "108049402",
+      nickname: "-",
+    },
+    {
+      id: "140408202",
+      nickname: "-",
+    },
+    {
+      id: "180353312",
+      nickname: "-",
+    },
+    {
+      id: "140413103",
+      nickname: "-",
+    },
+
+    //SADS
+    {
+      id: "140392633",
+      nickname: "-",
+    },
+    {
+      id: "140397253",
+      nickname: "-",
+    },
+    {
+      id: "108617173",
+      nickname: "-",
+    },
+    {
+      id: "140410293",
+      nickname: "-",
+    },
+    {
+      id: "124744824",
+      nickname: "-",
+    },
+    {
+      id: "140395844",
+      nickname: "-",
+    },
+    {
+      id: "117551454",
+      nickname: "-",
+    },
+    {
+      id: "229474664",
+      nickname: "-",
+    },
+    {
+      id: "180352965",
+      nickname: "-",
+    },
+    {
+      id: "125693585",
+      nickname: "-",
+    },
+    {
+      id: "98695236",
+      nickname: "-",
+    },
+    {
+      id: "140397866",
+      nickname: "-",
+    },
+    {
+      id: "43704117",
+      nickname: "-",
+    },
+    {
+      id: "140388407",
+      nickname: "-",
+    },
+    {
+      id: "140389647",
+      nickname: "-",
+    },
+    {
+      id: "164747567",
+      nickname: "-",
+    },
+    {
+      id: "181665067",
+      nickname: "-",
+    },
+    {
+      id: "180613328",
+      nickname: "-",
+    },
+    {
+      id: "113065738",
+      nickname: "-",
+    },
+
+    //SADSA
+    {
+      id: "121163638",
+      nickname: "-",
+    },
+    {
+      id: "140404868",
+      nickname: "-",
+    },
+    {
+      id: "140400278",
+      nickname: "-",
+    },
+    {
+      id: "135912429",
+      nickname: "-",
+    },
+    {
+      id: "180797939",
+      nickname: "-",
+    },
+    {
+      id: "140394069",
+      nickname: "-",
+    },
+    {
+      id: "140386389",
+      nickname: "-",
+    },
+    {
+      id: "184706273",
+      nickname: "-",
+    },
+    {
+      id: "191167048",
+      nickname: "-",
+    },
+    {
+      id: "229415560",
+      nickname: "-",
+    },
+    {
+      id: "229471804",
+      nickname: "-",
+    },
+    {
+      id: "181660132",
+      nickname: "-",
+    },
+    {
+      id: "181660857",
+      nickname: "-",
+    },
+    {
+      id: "39594241",
+      nickname: "-",
+    },
+    {
+      id: "254080060",
+      nickname: "-",
+    },
+    {
+      id: "241355556",
+      nickname: "-",
+    },
+    {
+      id: "181659333",
+      nickname: "-",
+    },
+    {
+      id: "181669248",
+      nickname: "-",
+    },
+    {
+      id: "180624238",
+      nickname: "-",
+    },
+    {
+      id: "39449030",
+      nickname: "-",
+    },
+    //SADSA
+    {
+      id: "258855900",
+      nickname: "-",
+    },
+    {
+      id: "241351375",
+      nickname: "-",
+    },
+    {
+      id: "181662268",
+      nickname: "-",
+    },
+    {
+      id: "125573588",
+      nickname: "-",
+    },
+    {
+      id: "180614419",
+      nickname: "-",
+    },
+    {
+      id: "140346699",
+      nickname: "-",
+    },
+    {
+      id: "217549705",
+      nickname: "-",
+    },
+    {
+      id: "121710782",
+      nickname: "-",
+    },
+    {
+      id: "238825447",
+      nickname: "-",
+    },
+    {
+      id: "180608868",
+      nickname: "-",
+    },
+    {
+      id: "161818709",
+      nickname: "-",
+    },
+    {
+      id: "140403776",
+      nickname: "-",
+    },
+    {
+      id: "247071223",
+      nickname: "-",
+    },
+    {
+      id: "180353790",
+      nickname: "-",
+    },
+    {
+      id: "164043554",
+      nickname: "-",
+    },
+    {
+      id: "140405956",
+      nickname: "-",
+    },
+    {
+      id: "163965987",
+      nickname: "-",
+    },
+    {
+      id: "180621687",
+      nickname: "-",
+    },
+    {
+      id: "140391259",
+      nickname: "-",
+    },
+    {
+      id: "221417173",
+      nickname: "-",
+    },
+    {
+      id: "140403296",
+      nickname: "-",
+    },
+    {
+      id: "233186173",
+      nickname: "-",
+    },
+    {
+      id: "139562895",
+      nickname: "-",
+    },
+    {
+      id: "241346471",
+      nickname: "-",
+    },
+    {
+      id: "180611788",
+      nickname: "-",
+    },
+    {
+      id: "139419367",
+      nickname: "-",
+    },
+    {
+      id: "180625844",
+      nickname: "-",
+    },
+    {
+      id: "224290173",
+      nickname: "-",
+    },
+    {
+      id: "140296453",
+      nickname: "-",
+    },
+    {
+      id: "181665973",
+      nickname: "-",
+    },
+    {
+      id: "180606995",
+      nickname: "-",
+    },
+
+    {
+      id: "115530082",
+      nickname: "-",
+    },
+    {
+      id: "229554254",
+      nickname: "-",
+    },
+    {
+      id: "281797770",
+      nickname: "-",
+    },
+    {
+      id: "232253440",
+      nickname: "-",
+    },
+    {
+      id: "140055696",
+      nickname: "-",
+    },
+    {
+      id: "204873972",
+      nickname: "-",
+    },
+    {
+      id: "282442611",
+      nickname: "-",
+    },
+    {
+      id: "249613979",
+      nickname: "-",
+    },
+    {
+      id: "282441043",
+      nickname: "-",
+    },
+    {
+      id: "282444034",
+      nickname: "-",
+    },
+    {
+      id: "269842511",
+      nickname: "-",
+    },
+    {
+      id: "164006605",
+      nickname: "-",
+    },
+    {
+      id: "140396609",
+      nickname: "-",
+    },
+    {
+      id: "180623052",
+      nickname: "-",
+    },
+    {
+      id: "222580070",
+      nickname: "-",
+    },
+    {
+      id: "236254564",
+      nickname: "-",
+    },
+    {
+      id: "140405438",
+      nickname: "-",
+    },
+    {
+      id: "140411027",
+      nickname: "-",
+    },
+    {
+      id: "140393275",
+      nickname: "-",
+    },
+    {
+      id: "283469701",
+      nickname: "-",
+    },
+    {
+      id: "140404384",
+      nickname: "-",
+    },
+
+    {
+      id: "283466355",
+      nickname: "-",
+    },
+    {
+      id: "140401600",
+      nickname: "-",
+    },
+    {
+      id: "221656920",
+      nickname: "-",
+    },
+    {
+      id: "139469150",
+      nickname: "-",
+    },
+    {
+      id: "234811412",
+      nickname: "-",
+    },
+    {
+      id: "180594667",
+      nickname: "-",
+    },
+    {
+      id: "138572787",
+      nickname: "-",
+    },
+    {
+      id: "304625875",
+      nickname: "-",
+    },
+    {
+      id: "283471384",
+      nickname: "-",
+    },
+
+    {
+      id: "276357212",
+      nickname: "-",
+    },
+    {
+      id: "140399136",
+      nickname: "-",
+    },
+    {
+      id: "324700115",
+      nickname: "-",
+    },
+    {
+      id: "324383854",
+      nickname: "-",
+    },
+
+    {
+      id: "229553426",
+      nickname: "-",
+    },
+
+    {
+      id: "241328586",
+      nickname: "-",
+    },
+    {
+      id: "282458824",
+      nickname: "-",
+    },
+    {
+      id: "296392585",
+      nickname: "-",
+    },
+    {
+      id: "330945281",
+      nickname: "-",
+    },
+    {
+      id: "331690470",
+      nickname: "-",
+    },
+    {
+      id: "223004332",
+      nickname: "-",
+    },
+    {
+      id: "224352588",
+      nickname: "-",
+    },
+    {
+      id: "241349809",
+      nickname: "-",
+    },
+    {
+      id: "140388982",
+      nickname: "-",
+    },
+    {
+      id: "220328923",
+      nickname: "-",
+    },
+
+    {
+      id: "225823441",
+      nickname: "-",
+    },
+    {
+      id: "177543994",
+      nickname: "-",
+    },
+    {
+      id: "223166981",
+      nickname: "-",
+    },
+    {
+      id: "229634355",
+      nickname: "-",
+    },
+    {
+      id: "279493408",
+      nickname: "-",
+    },
+    {
+      id: "282445207",
+      nickname: "-",
+    },
+
+    {
+      id: "240788820",
+      nickname: "-",
+    },
+    {
+      id: "181658430",
+      nickname: "-",
+    },
+    {
+      id: "225292790",
+      nickname: "-",
+    },
+    {
+      id: "140391971",
+      nickname: "-",
+    },
+    {
+      id: "180615440",
+      nickname: "-",
+    },
+
+    //SADS
+    {
+      id: "140387162",
+      nickname: "-",
+    },
+    {
+      id: "173064313",
+      nickname: "-",
+    },
+    {
+      id: "181666513",
+      nickname: "-",
+    },
+    {
+      id: "181661623",
+      nickname: "-",
+    },
+
+    // {
+    //   id: "178117533",
+    //   nickname: "-",
+    // },
+    {
+      id: "233522154",
+      nickname: "-",
+    },
+    {
+      id: "140387825",
+      nickname: "-",
+    },
+    {
+      id: "140095935",
+      nickname: "-",
+    },
+    {
+      id: "140398535",
+      nickname: "-",
+    },
+
+    {
+      id: "181663037",
+      nickname: "-",
+    },
+    {
+      id: "180610218",
+      nickname: "-",
+    },
+
+    //SADSA
+
+    {
+      id: "139945479",
+      nickname: "-",
+    },
+
+    {
+      id: "241348117",
+      nickname: "-",
+    },
+    {
+      id: "241340974",
+      nickname: "-",
+    },
+    {
+      id: "241334831",
+      nickname: "-",
+    },
+    {
+      id: "258608075",
+      nickname: "-",
+    },
+    {
+      id: "260663624",
+      nickname: "-",
+    },
+
+    {
+      id: "281076601",
+      nickname: "-",
+    },
+    {
+      id: "282455025",
+      nickname: "-",
+    },
+    {
+      id: "349255630",
+      nickname: "-",
+    },
+    {
+      id: "354422420",
+      nickname: "-",
+    },
+    {
+      id: "283455692",
+      nickname: "-",
+    },
+
+    {
+      id: "240106744",
+      nickname: "-",
+    },
+    {
+      id: "378216196",
+      nickname: "-",
+    },
+    {
+      id: "381795745",
+      nickname: "-",
+    },
+    {
+      id: "385869250",
+      nickname: "-",
+    },
+    {
+      id: "373136885",
+      nickname: "-",
+    },
+    {
+      id: "385859719",
+      nickname: "-",
+    },
+    {
+      id: "373813008",
+      nickname: "-",
+    },
+    {
+      id: "362820336",
+      nickname: "-",
+    },
+    {
+      id: "366929057",
+      nickname: "-",
+    },
+    {
+      id: "373114613",
+      nickname: "-",
+    },
+    {
+      id: "389827971",
+      nickname: "-",
+    },
+    {
+      id: "389065045",
+      nickname: "-",
+    },
+    {
+      id: "389079856",
+      nickname: "-",
+    },
+    {
+      id: "389098001",
+      nickname: "-",
+    },
+    {
+      id: "389143116",
+      nickname: "-",
+    },
+    {
+      id: "389034443",
+      nickname: "-",
+    },
+    {
+      id: "389704148",
+      nickname: "-",
+    },
+    {
+      id: "389720306",
+      nickname: "-",
+    },
+    {
+      id: "389736301",
+      nickname: "-",
+    },
+    {
+      id: "410937993",
+      nickname: "-",
+    },
+    {
+      id: "411740929",
+      nickname: "-",
+    },
+    {
+      id: "411833686",
+      nickname: "-",
+    },
+    {
+      id: "412326211",
+      nickname: "-",
+    },
+    {
+      id: "411491009",
+      nickname: "-",
+    },
+    {
+      id: "412648892",
+      nickname: "-",
+    },
+    {
+      id: "413298673",
+      nickname: "-",
+    },
+    {
+      id: "413450378",
+      nickname: "-",
+    },
+    {
+      id: "414903267",
+      nickname: "-",
+    },
+    {
+      id: "414989854",
+      nickname: "-",
+    },
+    {
+      id: "415067742",
+      nickname: "-",
+    },
+    {
+      id: "415876806",
+      nickname: "-",
+    },
+    {
+      id: "416069768",
+      nickname: "-",
+    },
+    {
+      id: "406106974",
+      nickname: "-",
+    },
+    {
+      id: "411473888",
+      nickname: "-",
+    },
+    {
+      id: "416861731",
+      nickname: "-",
+    },
+    {
+      id: "418189111",
+      nickname: "-",
+    },
+    {
+      id: "412526905",
+      nickname: "-",
+    },
+    {
+      id: "419993345",
+      nickname: "-",
+    },
+    {
+      id: "414149531",
+      nickname: "-",
+    },
+    {
+      id: "414312438",
+      nickname: "-",
+    },
+    {
+      id: "420060324",
+      nickname: "-",
+    },
+    {
+      id: "421001004",
+      nickname: "-",
+    },
+    {
+      id: "420265582",
+      nickname: "-",
+    },
+    {
+      id: "421095508",
+      nickname: "-",
+    },
+    {
+      id: "422014333",
+      nickname: "-",
+    },
+    {
+      id: "418657252",
+      nickname: "-",
+    },
+    {
+      id: "421971222",
+      nickname: "-",
+    },
+    {
+      id: "419091024",
+      nickname: "-",
+    },
+    {
+      id: "422242626",
+      nickname: "-",
+    },
+  ],
+};
+
+const tableAccount = document.getElementById("table-account");
+
+for (let i = 0; i < account.account.length; i++) {
+  const id = account.account[i].id;
+  const nickname = account.account[i].nickname;
+  const row = document.createElement("tr");
+  const colCheck = document.createElement("td");
+  const divContainerCheck = document.createElement("div");
+  const divCheck = document.createElement("div");
+  const check = document.createElement("input");
+
+  const colId = document.createElement("td");
+  const divId = document.createElement("div");
+  const pId = document.createElement("p");
+
+  const colNick = document.createElement("td");
+  colNick.setAttribute("class", "");
+
+  const divNick = document.createElement("div");
+  divNick.setAttribute("class", "flex items-center");
+
+  const pNick = document.createElement("p");
+  pNick.setAttribute("class", "text-sm leading-none text-gray-600 ml-2");
+
+  pNick.innerHTML = i + 1;
+
+  divId.setAttribute("class", "flex items-center pl-5");
+  pId.setAttribute("class", "text-base leading-none text-gray-700 mr-2");
+
+  const colCopy = document.createElement("td");
+  colCopy.setAttribute("class", "flex flex-row justify-end mr-2 py-2");
+
+  const btnCopy = document.createElement("button");
+  btnCopy.setAttribute(
+    "class",
+    "ml-auto focus:ring-2 focus:ring-offset-2 focus:ring-red-300 text-sm leading-none text-gray-600 py-3 px-5 bg-gray-100 rounded hover:bg-gray-200 focus:outline-none"
+  );
+  btnCopy.setAttribute("id", "btn-copy" + i);
+
+  if (id == "135912429") {
+    pId.setAttribute("class", "bg-red-300");
+    pId.setAttribute(
+      "style",
+      "text-decoration-line: line-through; text-decoration-color: blue;"
+    );
+  }
+
+  btnCopy.innerHTML = "Copy";
+
+  colCopy.appendChild(btnCopy);
+
+  pId.innerHTML = id;
+  divId.appendChild(pId);
+  colId.appendChild(divId);
+
+  divNick.appendChild(pNick);
+  colNick.appendChild(divNick);
+
+  check.setAttribute("type", "checkbox");
+  check.setAttribute("id", "check" + i);
+  check.setAttribute("name", "check" + i);
+  check.setAttribute(
+    "class",
+    "focus:opacity-100 checkbox absolute cursor-pointer w-full h-full"
+  );
+
+  divContainerCheck.setAttribute("class", "ml-5");
+  divCheck.setAttribute(
+    "class",
+    "bg-gray-200 rounded-sm w-5 h-5 flex flex-shrink-0 justify-center items-center relative"
+  );
+
+  divCheck.appendChild(check);
+  divContainerCheck.appendChild(divCheck);
+
+  row.setAttribute("tabindex", i);
+  row.setAttribute("id", "row" + i);
+  row.setAttribute("class", "focus:outline-none rounded");
+  colCheck.appendChild(divContainerCheck);
+  // col.innerHTML = nickname;
+  row.appendChild(colNick);
+  row.appendChild(colId);
+  row.appendChild(colCheck);
+  row.appendChild(colCopy);
+  tableAccount.appendChild(row);
+
+  const isChecked = document.getElementById("check" + i);
+
+  btnCopy.addEventListener("click", () => {
+    navigator.clipboard.writeText(id);
+    isChecked.checked = true;
+    document.getElementById("row" + i).classList.add("bg-indigo-200");
+  });
+
+  isChecked.addEventListener("change", () => {
+    document.getElementById("row" + i).classList.remove("bg-indigo-200");
+  });
+}
