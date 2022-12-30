bf4cd42204221dde6622122e5843844419d0f96f
ZainiNijar <90563272+ZainiNijar@users.noreply.github.com>
2022-12-30 14:35:59 +0700

    Update lim-10b.js
    shhs

--- src/js/lim-10b.js
+++ src/js/lim-10b.js
@@ -73,6 +73,7 @@ const tableAccount = document.getElementById("table-account");
 for (let i = 0; i < account.account.length; i++) {
   const id = account.account[i].id;
   const nickname = account.account[i].nickname;
+
   const row = document.createElement("tr");
   const colCheck = document.createElement("td");
   const divContainerCheck = document.createElement("div");
@@ -86,13 +87,23 @@ for (let i = 0; i < account.account.length; i++) {
   const colNick = document.createElement("td");
   colNick.setAttribute("class", "");
 
+  const colNumber = document.createElement("td");
+  colNumber.setAttribute("class", "");
+
+
   const divNick = document.createElement("div");
   divNick.setAttribute("class", "flex items-center");
+
+  const divNumber = document.createElement("div");
+  divNumber.setAttribute("class", "flex items-center");
 
   const pNick = document.createElement("p");
   pNick.setAttribute("class", "text-sm leading-none text-gray-600 ml-2");
 
-  pNick.innerHTML = i + 1;
+  const pNumber = document.createElement("p");
+  pNumber.setAttribute("class", "text-sm leading-none text-gray-600 ml-2");
+
+  pNumber.innerHTML = i + 1;
 
   divId.setAttribute("class", "flex items-center pl-5");
   pId.setAttribute("class", "text-base leading-none text-gray-700 mr-2");
@@ -139,7 +150,7 @@ for (let i = 0; i < account.account.length; i++) {
   row.setAttribute("id", "row" + i);
   row.setAttribute("class", "focus:outline-none rounded");
   colCheck.appendChild(divContainerCheck);
-  // col.innerHTML = nickname;
+  col.innerHTML = nickname;
   row.appendChild(colNick);
   row.appendChild(colId);
   row.appendChild(colCheck);
